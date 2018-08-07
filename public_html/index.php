ab83c78ca3668c2383ff81ea6c15c722153bb892
Carlos Marquez <carl.marq95@gmail.com>
2018-08-07 16:38:00 -0600

    after getting my commits to show on github i set up my boilerplate for this little project lol

---
+++ public_html/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+
+<html lang="en">
+	<head>
+		<meta charset="UTF-8"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+		<!--Font Awesome-->
+		<script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+		<!--JQuery first, Popper.js second, and Bootstrap Js  third-->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+				  integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+				  crossorigin="anonymous"></script>
+
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"
+				  integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T"
+				  crossorigin="anonymous"></script>
+
+		<title>unclelame</title>
+	</head>
+
+	<body>
+		<header> <!--NavBar-->
+
+		</header>
+
+		<section> <!--Media Player-->
+			<audio controls>
+
+			</audio>
+		</section>
+	</body>
+
+</html>
