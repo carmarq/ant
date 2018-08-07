b53fadd6e3809f5d1392e55b43ab15c61b7bc77f
Carlos Marquez <carl.marq95@gmail.com>
2018-08-07 16:08:15 -0600

    test???

--- index.php
+++ index.php
@@ -2,8 +2,9 @@
 
 <html lang="en">
 	<head>
-		<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
-		<title>Document</title>
+		<meta charset="UTF-8"/>
+		
+		<title>Document??</title>
 	</head>
 	<body>
 <p>lolplease</p>
